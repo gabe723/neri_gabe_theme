b9eb0954e1b75bedda22caf3ad3a5e61d10699de
gabe723 <gneri723@gmail.com>
2017-04-06 13:17:00 -0700

    update

--- wp-content/themes/amazing-shop-lite/functions.php
+++ wp-content/themes/amazing-shop-lite/functions.php
@@ -279,11 +279,11 @@ function amazing_shop_lite_price(){
 			}
 		}
 
-		//unhook WooCommerce wrappers
+		//remove hooks for WooCommerce wrappers
 		remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 		remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
-		//hook custom wrappers
+		//add hooks for custom wrappers
 		add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
 		add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
 
