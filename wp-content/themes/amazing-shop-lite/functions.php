c0345692395002d7dd007a0a8d3fbb062aad7974
gabe723 <gneri723@gmail.com>
2017-04-07 13:08:49 -0700

    updates

--- wp-content/themes/amazing-shop-lite/functions.php
+++ wp-content/themes/amazing-shop-lite/functions.php
@@ -283,7 +283,6 @@ function amazing_shop_lite_price(){
 		remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 		remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
-
 		//add hooks for custom wrappers
 		add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
 		function my_theme_wrapper_start() {
@@ -299,6 +298,15 @@ function amazing_shop_lite_price(){
 		add_action('after_setup_theme', 'woocommerce_support');
 		function woocommerce_support() {
 			add_theme_support('woocommerce');
+		}
+
+		//remove sidebar on "shop" archive page, but leave it everywhere else
+		add_action('template_redirect', 'gtn_remove_sidebar');
+		function gtn_remove_sidebar()
+		{
+			if ( is_post_type_archive('product') ) {
+				remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
+			}
 		}
 
 
