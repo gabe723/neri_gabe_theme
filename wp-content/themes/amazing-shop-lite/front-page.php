8dfc287c4579594b7ee2d1e344d5da39426127ef
gabe723 <gneri723@gmail.com>
2017-04-10 11:02:22 -0700

    update

---
+++ wp-content/themes/amazing-shop-lite/front-page.php
@@ -0,0 +1,80 @@
+<?php
+/**
+* The main template file
+*
+* This is the most generic template file in a WordPress theme
+* and one of the two required files for a theme (the other being style.css).
+* It is used to display a page when nothing more specific matches a query.
+* E.g., it puts together the home page when no home.php file exists.
+*
+* @link https://codex.wordpress.org/Template_Hierarchy
+*
+* @package WordPress
+* @subpackage Amazing_Shop_Lite
+* @since 1.0
+* @version 1.0
+*/
+
+define( 'WP_USE_THEMES', false ); get_header();
+?>
+<div class="wrapper">
+	<main id="content">
+		<?php //check to see if the slider plugin exists before running its function
+		if( function_exists( 'gn_slider' ) ){
+			gn_slider();
+		}
+		?>
+
+		<?php
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
+				?>
+
+				<h1 class="page-title">
+					We Have What You Need.
+				</h1>
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+
+				<?php
+			} // end while
+		} // end if
+		?>
+
+		<?php
+		//get up to 5 latest products
+		$products = new WP_Query( array(
+			'post_type'      => 'product',
+			'posts_per_page' => 5,
+		) );
+
+		if ( $products->have_posts() ) {
+			?>
+			<div class="featured-products">
+				<h2>Featured Products:</h2>
+				<ul>
+					<?php while( $products->have_posts() ){
+						$products->the_post();
+						?>
+						<li>
+							<a href="PERMALINK">
+								<?php the_post_thumbnail( 'thumbnail' ); ?>
+								<div class="caption">
+									<h3><?php the_title(); ?></h3>
+									<?php amazing_shop_lite_price(); ?>
+								</div>
+							</a>
+						</li>
+						<?php }//end while ?>
+					</ul>
+				</div>
+				<?php }//end of custom "product" query loop ?>
+			</main>
+			<!-- end #content -->
+
+		</div>
+		<!-- end .wrapper -->
+
+		<?php get_footer(); ?>
--- wp-content/themes/amazing-shop-lite/functions.php
+++ wp-content/themes/amazing-shop-lite/functions.php
@@ -332,6 +332,7 @@ function gtn_remove_sidebar()
 
 
 
+
 //remove each WooCommerce style one by one
 // add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
 // function jk_dequeue_styles($enqueue_styles) {
