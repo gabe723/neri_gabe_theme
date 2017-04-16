09b819cfc32c7585d4f6e6e43188c1a523f9acfe
gabe723 <gneri723@gmail.com>
2017-04-16 14:27:27 -0700

    update

--- wp-content/themes/amazing-shop-lite/front-page.php
+++ wp-content/themes/amazing-shop-lite/front-page.php
@@ -20,23 +20,12 @@ define( 'WP_USE_THEMES', false ); get_header();
 <div class="wrapper">
 	<main id="content">
 
-		<?php
-		if ( have_posts() ) {
-			while ( have_posts() ) {
-				the_post();
-				?>
-
 				<h1 class="page-title">
 					We Have What You Need.
 				</h1>
 				<div class="entry-content">
 					<?php the_content(); ?>
 				</div>
-
-				<?php
-			} // end while
-		} // end if
-		?>
 
 		<?php
 		//get up to 5 latest products
